7fa54555e8055d49be6d28036cfbf141c841894e
hkeerthi <hari.keerthi@gmail.com>
2016-10-08 16:35:33 -0400

    Add files via upload

---
+++ FoodInventory.Data/Models/DTOs/SalesDTO.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodInventory.Data.Models.DTOs
+{
+    public class SalesDTO
+    {       
+        public int SaleId { get; set; }
+
+        public Nullable<int> ProductId { get; set; }
+
+        public Nullable<int> UnitsSold { get; set; }
+
+        public Nullable<System.DateTime> SaleDate { get; set; }
+
+        public Nullable<int> Discount { get; set; }
+
+        public string PaymentType { get; set; }
+    }
+}
