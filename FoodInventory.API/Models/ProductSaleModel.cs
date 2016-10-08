53ee77d21132923b18abda96bee8e65b2b2a248e
hkeerthi <hari.keerthi@gmail.com>
2016-10-08 17:00:32 -0400

    Add files via upload

---
+++ FoodInventory.API/Models/ProductSaleModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodInventory.Models.API
+{
+    public class ProductSaleModel
+    {
+        public int SaleId { get; set; }
+
+        public Nullable<int> ProductId { get; set; }
+
+        public Nullable<int> UnitsSold { get; set; }
+
+        public Nullable<System.DateTime> SaleDate { get; set; }
+
+        public Nullable<int> Discount { get; set; }
+
+        public string PaymentType { get; set; }
+
+        public string ProductName { get; set; }
+        public Nullable<int> UnitsAvailable { get; set; }
+
+
+    }
+}
