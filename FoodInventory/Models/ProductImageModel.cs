fc38263625a11fbd02ea5f5d4ea5b123b4c1f8df
hkeerthi <hari.keerthi@gmail.com>
2016-10-08 16:23:17 -0400

    Add files via upload

---
+++ FoodInventory/Models/ProductImageModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodInventory.Models
+{    
+    public class ProductImageModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal SalesPrice { get; set; }
+        public Nullable<System.DateTime> SpoilDate { get; set; }
+        public int UnitsAvailable { get; set; }
+        public Nullable<System.DateTime> DeletedDate { get; set; }
+
+        public byte[] ProductImage { get; set; }
+
+        public List<ProductImageModel>ProductOptions{ get; set; }
+
+}
+}
---
+++ FoodInventory/Models/ProductSaleModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodInventory.Models
+{
+    public class ProductSaleModel
+    {
+        public int SaleId { get; set; }
+
+        public Nullable<int> ProductId { get; set; }
+
+        public Nullable<int> UnitsSold { get; set; }
+
+        public Nullable<System.DateTime> SaleDate { get; set; }
+
+        public Nullable<int> Discount { get; set; }
+
+        public string PaymentType { get; set; }
+
+        public List<ProductImageModel> ProductOptions { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<int> UnitsAvailable { get; set; }
+
+
+    }
+}
